bfe73b66dedb1bf3652631fa7ff58bcd5fbc3d1a
Sawal Timsina <52816353+sawal-timsina@users.noreply.github.com>
2024-09-02 17:46:43 +0545

    chore: send version parameter in itunes api

--- lib/core/functions/fetch_version.dart
+++ lib/core/functions/fetch_version.dart
@@ -93,6 +93,7 @@ Future<AppVersionData> fetchIOS(
   if (country != null) {
     parameters['country'] = country;
   }
+    parameters['version'] = 2;
   var uri = Uri.https(appleStoreAuthority, '/lookup', parameters);
   final response = await http.get(uri, headers: headers);
   if (response.statusCode == 200) {
