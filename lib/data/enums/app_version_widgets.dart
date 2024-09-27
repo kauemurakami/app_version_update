acd142edd9d678762526420a6e3bf40dda321478
Aashish-Dahal <ashishdahal490@gmail.com>
2024-09-27 13:12:35 +0545

    feat: version checker

--- lib/data/enums/app_version_widgets.dart
+++ lib/data/enums/app_version_widgets.dart
@@ -1,2 +1,7 @@
 /// Type displays modal.
 enum ShowModalType { page, bottomSheet, alertDialog }
+
+typedef VersionChecker = bool Function({
+  required String? localVersion,
+  required String? storeVersion,
+});
