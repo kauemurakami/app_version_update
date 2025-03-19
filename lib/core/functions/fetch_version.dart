2bc9457f06371ac81d24ee7d1e34eb9071501441
Kauê Murakami <kauetmurakami@gmail.com>
2025-03-19 20:44:47 -0300

    Try fix change version store in hot reload

--- lib/core/functions/fetch_version.dart
+++ lib/core/functions/fetch_version.dart
@@ -13,13 +13,11 @@ import 'convert_version.dart';
 /// * ```appleId``` unique identifier in Apple Store, if null, we will use your package name.
 /// * ```playStoreId``` unique identifier in Play Store, if null, we will use your package name.
 /// * ```country``` (iOS only) region of store, if null, we will use 'us'.
-Future<AppVersionData> fetchVersion(
-    {String? playStoreId, String? appleId, String? country}) async {
+Future<AppVersionData> fetchVersion({String? playStoreId, String? appleId, String? country}) async {
   final packageInfo = await PackageInfo.fromPlatform();
   AppVersionData data = AppVersionData();
   if (Platform.isAndroid) {
-    data =
-        await fetchAndroid(packageInfo: packageInfo, playStoreId: playStoreId);
+    data = await fetchAndroid(packageInfo: packageInfo, playStoreId: playStoreId);
   } else if (Platform.isIOS) {
     data = await fetchIOS(
       packageInfo: packageInfo,
@@ -29,56 +27,50 @@ Future<AppVersionData> fetchVersion(
   } else {
     throw "Unknown platform";
   }
-  data.canUpdate = await convertVersion(
-      version: data.localVersion, versionStore: data.storeVersion);
+  data.canUpdate = await convertVersion(version: data.localVersion, versionStore: data.storeVersion);
   return data;
 }
 
-Future<AppVersionData> fetchAndroid(
-    {PackageInfo? packageInfo, String? playStoreId}) async {
+Future<AppVersionData> fetchAndroid({PackageInfo? packageInfo, String? playStoreId}) async {
   playStoreId = playStoreId ?? packageInfo?.packageName;
 
   final parameters = {
     "id": playStoreId,
   };
   var uri = Uri.https(playStoreAuthority, playStoreUndecodedPath, parameters);
-  final response =
-      await http.get(uri, headers: headers).catchError((e) => throw e);
+  final response = await http.get(uri, headers: headers).catchError((e) => throw e);
   if (response.statusCode == 200) {
     final String htmlString = response.body;
 
     RegExp regex;
     Iterable<RegExpMatch> matches;
     // Use regex to find all occurrences of version in the format "]]],"<version-number>"
-    if (packageInfo!.version.split('.').length == 3) {
+    if (htmlString.contains('version')) {
+      regex = RegExp(r'(\d+\.\d+\.\d+)');
+      matches = regex.allMatches(htmlString);
+    } else if (packageInfo!.version.split('.').length == 3) {
       regex = RegExp(r'"\]\]\],null,null,null,\[\[\["(.*?)"\]\]\]');
       matches = regex.allMatches(htmlString);
     } else {
       regex = RegExp(r'"\]\]\],"(.*?)"');
       matches = regex.allMatches(htmlString);
     }
-    // Verify normal version
-    if (htmlString.contains('version')) {
-      regex = RegExp(r'(\d+\.\d+\.\d+)');
-      matches = regex.allMatches(htmlString);
-    }
 
     // Extract the last version found
     if (matches.isNotEmpty) {
       final lastMatch = matches.last;
-      String? lastVersion = lastMatch.group(1);
+      String? lastVersion = lastMatch.group(0);
       lastVersion = lastVersion!.split('"').first;
-      print(lastVersion);
       if (kDebugMode) {
         print(
-          'Versão local ${packageInfo.version} Última versão encontrada: $lastVersion',
+          'Versão local ${packageInfo!.version} Última versão encontrada: $lastVersion',
         );
       }
       return AppVersionData(
         // canUpdate: packageInfo.version < lastVersion ? true : false,
         storeVersion: lastVersion,
         storeUrl: uri.toString(),
-        localVersion: packageInfo.version,
+        localVersion: packageInfo!.version,
         targetPlatform: TargetPlatform.android,
       );
     } else {
@@ -89,13 +81,9 @@ Future<AppVersionData> fetchAndroid(
   }
 }
 
-Future<AppVersionData> fetchIOS(
-    {PackageInfo? packageInfo, String? appleId, String? country}) async {
-  assert(appleId != null || packageInfo != null,
-      'One between appleId or packageInfo must not be null');
-  var parameters = (appleId != null)
-      ? {"id": appleId}
-      : {'bundleId': packageInfo?.packageName};
+Future<AppVersionData> fetchIOS({PackageInfo? packageInfo, String? appleId, String? country}) async {
+  assert(appleId != null || packageInfo != null, 'One between appleId or packageInfo must not be null');
+  var parameters = (appleId != null) ? {"id": appleId} : {'bundleId': packageInfo?.packageName};
   if (country != null) {
     parameters['country'] = country;
   }
